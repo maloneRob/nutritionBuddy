65d242c4838d11cb07a16d10a5086a096bc4e851
maloneRob <rmalon8@uic.edu>
2022-10-26 19:51:47 -0500

    implemented the api call for the restaurants by location added QueryRestaurants.java which handles the api call to get the restaurants and implemented a callback to get the JSON returned added MySingleton.java to handle the instance of the queue still need json parser classes and the location class, but added skeleton code where they should be implemented
    will implement the QueryMenu next along with parsing the data from the JSONs

--- Code/Project/app/src/main/java/com/G26/fitnessandnutritionbuddy/UserProfileFragment.java
+++ Code/Project/app/src/main/java/com/G26/fitnessandnutritionbuddy/UserProfileFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,19 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.G26.fitnessandnutritionbuddy.data.model.UserProfile;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +48,7 @@ public class UserProfileFragment extends Fragment {
     private String mParam2;
     UserProfile user;
     EditText userName, calories, fats, proteins, carbs;
+    JSONObject restaurantJSON = null;
 
     public UserProfileFragment() {
         // Required empty public constructor
@@ -92,6 +107,7 @@ public class UserProfileFragment extends Fragment {
         carbs = (EditText) view.findViewById(R.id.editTextTextUserProfileCarbs);
         fats = (EditText) view.findViewById(R.id.editTextTextUserProfileFats);
         proteins = (EditText) view.findViewById(R.id.editTextTextUserProfileProteins);
+        //find meal button
         Button button = view.findViewById(R.id.btnUserProfile);
 
         //fill in text fields
@@ -101,6 +117,8 @@ public class UserProfileFragment extends Fragment {
         carbs.setText(user.getNutrientGoal("carbohydrates").toString());
         fats.setText(user.getNutrientGoal("fats").toString());
         proteins.setText(user.getNutrientGoal("protein").toString());
+
+        //find meal button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -117,6 +135,65 @@ public class UserProfileFragment extends Fragment {
                 MainActivity activity = (MainActivity) getActivity();
                 activity.saveData(newGoals);
 
+                // todo
+                // get user location
+                //float lat = LocationClass.getLat();
+
+
+                double tmpLat = 41.5250;
+                double tmpLng = -88.0817;
+                //float lng = LocationClass.getLng()
+                //instantiate instance of QueryRestaurant class
+                QueryRestaurants query = new QueryRestaurants(getActivity());
+
+                // .queryLocation(float, float, new VolleyResponse listener(){)
+                // must implement the onError and onResponse methods
+                query.queryLocation(tmpLat, tmpLng, new QueryRestaurants.VolleyResponseListener() {
+                    @Override
+                    public void onError(String message) {
+                        // print error message
+                        Log.i("[RESPONSE]", message);
+                    }
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("[RESPONSE]", response.toString());
+                        restaurantJSON = response;
+                        Log.i("[Response object]", restaurantJSON.toString());
+
+                        if (restaurantJSON != null) {
+                            Log.i("[Response]", "about to start parsing the json");
+
+                            // todo
+                            // pass JSON to RestaurantParser here
+                            // RestaurauntParser rp = new RestaurantParser(restaurantJSON);
+
+                            // get the list of RestaurantObjects
+                            // ArrayList<Restaurant> restaurantsList = rp.getRestaurants();
+
+                            //todo
+                            // implement new class QueryMenu.java
+                            //will fix syntax
+                            /*for (Restaurant restaurants : restuarantsList){
+                                make seperate query for each restaurant
+                                parse the JSON returned
+                                get food objects and insert them into restuarant's menu
+
+                            }
+                            pass back arraylist of restaurant objects to the main activity to save to fragment
+                            put the restaurants on the gmap
+                            parse the menus to the listview
+                            */
+
+                        } else {
+                            Log.i("[RESPONSE]", "ERROR, EITHER NO RESTAURANTS OR ERROR IN METHOD");
+
+                        }
+
+                    }
+                });
+
+                //transition to the next fragment
                 navController.navigate(R.id.action_userProfileFragment_to_SecondFragment);
             }
         });
